9b13b941e6bd84d0a1c4cd192925e6ee8047783c
aagam342 <aagamj7229@gmail.com>
2024-11-19 15:08:03 +0530

    Check

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,7 +8,6 @@ const MainComponent = () => {
   if (!movies) return;
   const mainMovie = movies[0];
   //   console.log(mainMovie);
-
   const { original_title, overview, id } = mainMovie;
   return (
     <div className="pt-[30%] bg-black md:pt-0 ">
